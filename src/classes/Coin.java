0b79b1d46adf675dd60c957cd1a3700894315103
jkcadee <54956049+jkcadee@users.noreply.github.com>
2020-04-17 12:30:31 -0700

    comments for coin and star finished, collectible changed method name

--- src/classes/Coin.java
+++ src/classes/Coin.java
@@ -13,12 +13,20 @@ public class Coin implements Collectible {
     /* The shape of the Coin */
     private Ellipse shape;
 
-
+    /**
+     * Constructs a Coin object.
+     *
+     * @param value The value of the Coin.
+     * @param shape The Shape of the Coin.
+     */
     public Coin(int value, Ellipse shape) {
         this.value = value;
         this.shape = shape;
     }
 
+    /**
+     * Constructs the Coin with default values.
+     */
     public Coin() {
         Ellipse newCoin = new Ellipse(10, 7.5, 5, 5);
         this.value = 1;
@@ -26,21 +34,42 @@ public class Coin implements Collectible {
         this.shape.setFill(Color.GOLD);
     }
 
+    /**
+     * Sets the value of the Coin.
+     *
+     * @param value The value to be set.
+     */
     public void setValue(int value) {
         this.value = value;
     }
 
+    /**
+     * Sets the shape of the Coin.
+     *
+     * @param shape The shape to be set.
+     */
     public void setShape(Ellipse shape) {
         this.shape = shape;
     }
 
+    /**
+     * Gets the current value of the Coin.
+     *
+     * @return The value of the Coin.
+     */
     @Override
-    public int collect() {
+    public int getValue() {
         return value;
     }
 
+    /**
+     * Gets the current shape of the Coin.
+     *
+     * @return The shape of the Coin.
+     */
     @Override
     public Ellipse getShape() {
         return shape;
     }
+
 }
