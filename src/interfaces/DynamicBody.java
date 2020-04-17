69e5c7a7c185c37b14806d88b8082944e6dba196
jkcadee <54956049+jkcadee@users.noreply.github.com>
2020-04-17 13:56:31 -0700

    added parameters for addVx and addVy

--- src/interfaces/DynamicBody.java
+++ src/interfaces/DynamicBody.java
@@ -3,7 +3,7 @@ package interfaces;
 public interface DynamicBody extends PhysicalBody {
     double getVx();
     double getVy();
-    void addVx();
-    void addVy();
+    void addVx(double num);
+    void addVy(double num);
     void move();
 }
