27e034b2591511d613e1830efcf324d26fb7a4d5
jkcadee <54956049+jkcadee@users.noreply.github.com>
2020-04-17 11:03:53 -0700

    started Star

--- src/Classes/Star.java
+++ src/Classes/Star.java
@@ -1,4 +1,35 @@
 package Classes;
 
-public class Star {
+import interfaces.StaticBody;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Shape;
+
+public class Star implements StaticBody {
+    private final int radius;
+    private final int mass;
+    private final Ellipse shape;
+    private final boolean destructible;
+
+    public Star(int radius, int mass, Ellipse shape, boolean destructible) {
+        this.radius = radius;
+        this.mass = mass;
+        this.shape = shape;
+        this.destructible = destructible;
+
+    }
+
+    @Override
+    public int getMass() {
+        return mass;
+    }
+
+    @Override
+    public boolean getDestructible() {
+        return destructible;
+    }
+
+    @Override
+    public Shape getShape() {
+        return shape;
+    }
 }
