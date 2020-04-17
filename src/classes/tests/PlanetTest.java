c97ef5e0fffed2a7de6abdab95232504f234ade1
Go-Maun <gomaun55@gmail.com>
2020-04-17 15:49:00 -0700

    added move tests for PlanetTest

--- src/classes/tests/PlanetTest.java
+++ src/classes/tests/PlanetTest.java
@@ -1,6 +1,7 @@
 package classes.tests;
 
 import classes.Planet;
+import javafx.scene.shape.Circle;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -18,7 +19,7 @@ public class PlanetTest {
     @org.junit.Before
     public void setUp() throws Exception {
         defaultPlanet = new Planet();
-        testPlanet = new Planet(0, 0, -2.123, 5.345, true);
+        testPlanet = new Planet(0, 0, -2.123, 5.345, false);
     }
 
     @Test
@@ -63,6 +64,46 @@ public class PlanetTest {
     public void addVyAddsProperlyToVyOnTestPlanet(){
         testPlanet.addVy(3.2);
         assertEquals(8.545, testPlanet.getVy(), 0.05);
+    }
+
+    @Test
+    public void moveProperlyAffectsDefaultPlanetsCenterX() {
+        Circle oldDefaultPlanetCircle = (Circle) defaultPlanet.getShape();
+        double oldXPosition = oldDefaultPlanetCircle.getCenterX();
+        defaultPlanet.move();
+        Circle newDefaultPlanetCircle = (Circle) defaultPlanet.getShape();
+        double newXPosition = newDefaultPlanetCircle.getCenterX();
+        assertEquals(oldXPosition + defaultPlanet.getVx(), newXPosition, 0.05);
+    }
+
+    @Test
+    public void moveProperlyAffectsDefaultPlanetsCenterY() {
+        Circle oldDefaultPlanetCircle = (Circle) defaultPlanet.getShape();
+        double oldYPosition = oldDefaultPlanetCircle.getCenterY();
+        defaultPlanet.move();
+        Circle newDefaultPlanetCircle = (Circle) defaultPlanet.getShape();
+        double newYPosition = newDefaultPlanetCircle.getCenterY();
+        assertEquals(oldYPosition + defaultPlanet.getVy(), newYPosition, 0.05);
+    }
+
+    @Test
+    public void moveProperlyAffectsTestPlanetsCenterX() {
+        Circle oldTestPlanetCircle = (Circle) testPlanet.getShape();
+        double oldXPosition = oldTestPlanetCircle.getCenterX();
+        testPlanet.move();
+        Circle newTestPlanetCircle = (Circle) testPlanet.getShape();
+        double newXPosition = newTestPlanetCircle.getCenterX();
+        assertEquals(oldXPosition + testPlanet.getVx(), newXPosition, 0.05);
+    }
+
+    @Test
+    public void moveProperlyAffectsTestPlanetsCenterY() {
+        Circle oldTestPlanetCircle = (Circle) testPlanet.getShape();
+        double oldYPosition = oldTestPlanetCircle.getCenterY();
+        testPlanet.move();
+        Circle newTestPlanetCircle = (Circle) testPlanet.getShape();
+        double newYPosition = newTestPlanetCircle.getCenterY();
+        assertEquals(oldYPosition + testPlanet.getVy(), newYPosition, 0.05);
     }
 
 }
