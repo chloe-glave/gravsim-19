4ddaeda230ba045c695c25bb51ea499f308a41a9
Chloe (Desktop) <cragzu@gmail.com>
2020-04-17 19:26:06 -0700

    Implemented basic click detection (ClickHandler)

--- src/classes/ClickHandler.java
+++ src/classes/ClickHandler.java
@@ -1,5 +1,6 @@
 package classes;
 
+import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 
 /**
@@ -12,11 +13,14 @@ import javafx.scene.input.MouseEvent;
  *
  * @version 2020
  */
-public class ClickHandler {
+public class ClickHandler implements EventHandler<MouseEvent> {
 
-    public void processMouseClick(MouseEvent event) {
-        double clickX = event.getX();
-        double clickY = event.getY();
+    @Override
+    public void handle(MouseEvent mouseEvent) { // todo: @Kayden add whatever click stuff u want
+        double clickX = mouseEvent.getX();
+        double clickY = mouseEvent.getY();
+
+        System.out.println("mouse click detected! " + clickX + " " + clickY);
     }
 
 }
