37f99e42c951f08d72a2db946a786b6a9aa0df49
Go-Maun <gomaun55@gmail.com>
2020-04-17 21:32:13 -0700

    finished StarTest juint tests

--- src/classes/tests/StarTest.java
+++ src/classes/tests/StarTest.java
@@ -1,6 +1,7 @@
 package classes.tests;
 
 import classes.Star;
+import javafx.scene.shape.Circle;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.*;
@@ -48,6 +49,64 @@ public class StarTest {
     @Test
     public void getDestructibleReturnsProperValueForTestStar() {
         assertTrue(testStar.getDestructible());
+    }
+
+    @Test
+    public void getShapeReturnsANonNullForDefaultStar() {
+        assertNotNull(defaultStar.getShape());
+    }
+
+    @Test
+    public void getShapeReturnsANonNullForTestStar() {
+        assertNotNull(testStar.getShape());
+    }
+
+    @Test
+    public void setXChangesXLocationForDefaultStar() {
+        defaultStar.setX(50.5);
+        assertEquals(defaultStar.getX(), 50.5, 0.05);
+    }
+
+    @Test
+    public void setYChangesYLocationForDefaultStar() {
+        defaultStar.setY(50.5);
+        assertEquals(defaultStar.getY(), 50.5, 0.05);
+    }
+
+    @Test
+    public void setXChangesXLocationForTestStar() {
+        testStar.setX(50.5);
+        assertEquals(testStar.getX(), 50.5, 0.05);
+    }
+
+    @Test
+    public void setYChangesYLocationForTestStar() {
+        testStar.setY(50.5);
+        assertEquals(testStar.getY(), 50.5, 0.05);
+    }
+
+    @Test
+    public void getXReturnsProperValueForDefaultStar() {
+        Circle star = (Circle) defaultStar.getShape();
+        assertEquals(defaultStar.getX(), star.getCenterX(), 0.05);
+    }
+
+    @Test
+    public void getYReturnsProperValueForDefaultStar() {
+        Circle star = (Circle) defaultStar.getShape();
+        assertEquals(defaultStar.getY(), star.getCenterY(), 0.05);
+    }
+
+    @Test
+    public void getXReturnsProperValueForTestStar() {
+        Circle star = (Circle) testStar.getShape();
+        assertEquals(testStar.getX(), star.getCenterX(), 0.05);
+    }
+
+    @Test
+    public void getYReturnsProperValueForTestStar() {
+        Circle star = (Circle) testStar.getShape();
+        assertEquals(testStar.getY(), star.getCenterY(), 0.05);
     }
 
 }
